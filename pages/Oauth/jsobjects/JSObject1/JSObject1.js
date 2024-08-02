cd1c28d3c993be2e4821f54c3d2a449cd8bd4635
Aparna Ramachandran <aparna@appsmith.com>
2024-08-02 08:19:33 +0000

    rwrw

---
+++ pages/Oauth/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFunss1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFuns2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
